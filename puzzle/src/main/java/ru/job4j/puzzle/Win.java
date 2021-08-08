2008b04e02529291007ee6e848535aabce8ddfc0
RaduKostashchuk <kostasc@mail.ru>
2021-08-08 08:36:17 +0700

    2. Сокобан. Тестовое задание. [#75283 #187064]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,31 +1,9 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean checkDiag(int[][] board) {
-        boolean result = true;
-        for (int index = 1; index < board.length; index++) {
-            if (board[index][index] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public static boolean checkBackDiag(int[][] board) {
-        boolean result = true;
-        for (int index = 1; index < board.length; index++) {
-            if (board[index][board.length - 1 - index] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
     public static boolean checkColumn(int[][] board, int cell) {
         boolean result = true;
-        for (int row = 1; row < board.length; row++) {
+        for (int row = 0; row < board.length; row++) {
             if (board[row][cell] != 1) {
                 result = false;
                 break;
@@ -36,7 +14,7 @@ public class Win {
 
     public static boolean checkRow(int[][] board, int row) {
         boolean result = true;
-        for (int cell = 1; cell < board[row].length; cell++) {
+        for (int cell = 0; cell < board[row].length; cell++) {
             if (board[row][cell] != 1) {
                 result = false;
                 break;
@@ -47,32 +25,14 @@ public class Win {
 
     public static boolean check(int[][] board) {
         boolean rsl = true;
-        // Check diag
-        if (board[0][0] == 1) {
-            if (checkDiag(board)) {
+         // Check row and column
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1
+                    && (checkColumn(board, index) || checkRow(board, index))) {
                 return rsl;
             }
         }
-        // Check backward diag
-        if (board[0][board.length - 1] == 1) {
-            if (checkBackDiag(board)) {
-                return rsl;
-            }
-        }
-        // Check row and column
-        for (int index = 0; index < board[0].length; index++) {
-            if (board[0][index] == 1) {
-                if(checkColumn(board, index)) {
-                    return rsl;
-                }
-            }
 
-            if (board[index][0] == 1) {
-                if(checkRow(board, index)) {
-                    return rsl;
-                }
-            }
-        }
         return rsl = false;
     }
 }
